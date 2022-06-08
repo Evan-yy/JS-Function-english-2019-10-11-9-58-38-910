1561efde7dfb8d70d6f7dce5d288e2a3c7b34870
Evan-yy <jjjklj@qq.com>
2022-06-08 14:23:14 +0800

    function4

---
+++ 4.function4/index.js
@@ -0,0 +1,7 @@
+function countWords(message){
+  // wirte your code here
+  var strArray = message.split(" ");
+  var sum = strArray.length;
+return sum;
+}
+countWords('Good morning, I love JavaScript.');
